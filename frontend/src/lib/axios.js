16bb9c3fd6429cc74eef50d3ed3d8ea6da14d6dc
subhajitgope <subhajitgope5@gmail.com>
2025-09-07 23:04:54 +0530

    final commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5012/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5012/api"   // local backend
+    : import.meta.env.VITE_API_URL + "/api";  // production backend on Render
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
